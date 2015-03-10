b8a4e295bfb3737599156d2788b12cd504102b93
Alex Lanza <alexlanza@no-reply.com>
2015-03-09 22:19:06 -0700

    moved authform to forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
-
+class AuthForm(AuthenticationForm):
+    def confirm_login_allowed(self, user):
+    	# auths everyone with valid creds, could be pickier
+        pass
+		
+		
 class SignupForm(forms.Form):
 	first_name = forms.CharField(label = "First Name")
 	last_name = forms.CharField(label = "Last Name")
