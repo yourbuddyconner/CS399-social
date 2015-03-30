8c2d04151c0074e25321562efaea987227c6d532
Alex Lanza <alexlanza@no-reply.com>
2015-03-29 19:01:59 -0700

    Added admin, removed signup
    Added admin, removed signup

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
-from . import views
+#from . import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,8 +14,7 @@ urlpatterns = patterns('',
     url(r'^explore', 'app.views.explore', name='explore'),
     url(r'^dashboard', 'app.views.dashboard', name='dashboard'),
     url(r'^login', 'app.views.login', name='login'),
-    url(r'^signup', 'app.views.signup', name='signup'),
     url(r'^logout', 'app.views.logout', name='logout'),
 
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
