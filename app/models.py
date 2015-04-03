331902b2ac940b9f544b7d7b208b43447831546f
SuitPotato <Erekyth@yahoo.com>
2015-04-02 23:06:30 -0700

    models pep8 compliance

--- app/models.py
+++ app/models.py
@@ -4,22 +4,20 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
-	
-	owner = models.ForeignKey(User);
-	caption = models.CharField(max_length=300)
-	timestamp = models.DateTimeField(auto_now_add=True)
-	
-	class Meta:
-		ordering = ('-timestamp',)
-	
 
-	
-	
+    owner = models.ForeignKey(User)
+    caption = models.CharField(max_length=300)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-timestamp',)
+
+
 class Image(models.Model):
-	image = models.FileField(upload_to = '/app/static/images')
+    image = models.FileField(upload_to='/app/static/images')
 
-	# returns a friendly name for django
-	def __unicode__(self):
-		return self.post_type
-
+    # returns a friendly name for django
+    def __unicode__(self):
+        return self.post_type
